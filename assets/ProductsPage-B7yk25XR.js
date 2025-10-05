306977dd3a1bc215417c483147cc646a71d46ef9
Jaime Sandoval <jssandoval@gmail.com>
2025-10-05 13:41:38 -0600

    Cambios al 20251005 v3

---
+++ assets/ProductsPage-B7yk25XR.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BjBewBOf.js";import{S as e,a as t}from"./PapersPage-BK3jxeup.js";function s(){return r.jsx(r.Fragment,{children:"Productos"})}function a(){return r.jsxs(r.Fragment,{children:[r.jsx(s,{}),r.jsx(e,{}),r.jsx(t,{})]})}export{a as default};
